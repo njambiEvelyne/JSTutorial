fa5ef0e4b6681a032453c68f9addc68d50017fd0
njambiEvelyne <evelynenganga76@gmail.com>
2025-07-02 08:51:54 +0300

    Added JSON features

--- js2assignment/js/main.js
+++
@@ -1,4 +0,0 @@
-const name = "Evelyne";
-console.log(Math.random());
-console.log(name.charAt(Math.floor(Math.random() * name.length)));
-console.log(name.charAt(Math.floor(Math.random() * name.length)));console.log(name.charAt(Math.floor(Math.random() * name.length)));
