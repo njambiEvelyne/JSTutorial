8bec309c64a7854aa16065721275dfaba5212eb5
njambiEvelyne <evelynenganga76@gmail.com>
2025-07-02 12:49:15 +0300

    Converted objects to receive JSON

--- js13/js/main.js
+++ js13/js/main.js
@@ -4,3 +4,23 @@ JSON is used to send and receive data.
 JSON is text format that is completely language independent.
 Meaning JSON is used to send & receive data in many languages .... not just in javascript.
 */
+const myObj = {
+  name: "Dave",
+  hobbies: ["Eat", "Sleep", "code"],
+  hello : function (){
+    console.log("Hello!");
+  }
+} 
+console.log(myObj);
+console.log(myObj.name);
+console.log(myObj.hobbies);
+myObj.hello();
+console.log(typeof myObj);
+
+const sendJSON = JSON.stringify(myObj);
+console.log(sendJSON);
+console.log(typeof sendJSON);
+
+const receiveJSON = JSON.parse(sendJSON);
+console.log(receiveJSON);
+console.log(typeof receiveJSON);
