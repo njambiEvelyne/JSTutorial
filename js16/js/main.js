88613554c828ecabf672d723d85151234f915332
njambiEvelyne <evelynenganga76@gmail.com>
2025-07-10 12:18:44 +0300

    Added the unary operators

--- js16/js/main.js
+++ js16/js/main.js
@@ -1,5 +1,5 @@
 // //DOM: Document Object Model
-// const view1 = document.getElementById("view1");
+// const view = document.getElementById("view1");
 // console.log(view1);
 
 // const view2 = document.querySelector("#view2");
@@ -27,3 +27,27 @@ for (i=0; i<10; i++){
   }
   console.log(i);
 }
+
+let nr1 = 12;
+let nr2 = 14;
+let result = nr1 + nr2;
+console.log(result);
+
+console.log("Hello, what's our name? Is it \"Mike\"?");
+let nr3 = 30;
+let nr4 = 5;
+let result1 = nr3 / nr4;
+console.log(result1);
+
+
+//Exponentiation
+let nr5 = 2;
+let nr6 = 3;
+let result2 = nr5 ** nr6;
+console.log(result2);
+
+//Modulus
+let numi = 7;
+let num2 = 2;
+let ans = numi % num2;
+console.log(`${numi} % ${num2} = ${ans}`);
