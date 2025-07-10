37a83982d100822ec309f224261df31d48ca30f0
njambiEvelyne <evelynenganga76@gmail.com>
2025-07-10 09:52:43 +0300

    Added new folder

---
+++ js15 copy/js/main.js
@@ -0,0 +1,29 @@
+// //DOM: Document Object Model
+// const view1 = document.getElementById("view1");
+// console.log(view1);
+
+// const view2 = document.querySelector("#view2");
+// console.log(view2);
+
+// //To change the style to show/hide the different views
+// //1. Showing view1 and hiding view2
+// view1.style.display = "flex";
+// view2.style.display = "none";
+
+// //2.//Showing view2 while hiding view1
+// view1.style.display = "none";
+// view2.style.display = "flex";
+
+
+// const views = document.getElementsByClassName("views");
+// console.log(views);
+
+// const sameView = document.querySelectorAll("view");
+// console.log(sameView); 
+
+for (i=0; i<10; i++){
+  if (i ===3){
+    break;
+  }
+  console.log(i);
+}
